1113c559a5c880db68433be6f8b3236f15a189b9
AtsukiYokota <atsuki.yokota@gmail.com>
2019-07-17 17:33:32 +0900

    Apply clang auto formatting

--- src/shapes.cpp
+++ src/shapes.cpp
@@ -1,13 +1,13 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
-* 
+*
 *  Copyright (c) 2008, Willow Garage, Inc.
 *  All rights reserved.
-* 
+*
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
 *  are met:
-* 
+*
 *   * Redistributions of source code must retain the above copyright
 *     notice, this list of conditions and the following disclaimer.
 *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
 *   * Neither the name of the Willow Garage nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
-* 
+*
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -37,57 +37,56 @@
 #include "robot_self_filter/shapes.h"
 namespace robot_self_filter
 {
-
-shapes::Shape* shapes::cloneShape(const shapes::Shape *shape)
+shapes::Shape* shapes::cloneShape(const shapes::Shape* shape)
 {
-    shapes::Shape *result = NULL;
-    switch (shape->type)
-    {
+  shapes::Shape* result = NULL;
+  switch (shape->type)
+  {
     case SPHERE:
-	result = new Sphere(static_cast<const Sphere*>(shape)->radius);
-	break;
+      result = new Sphere(static_cast<const Sphere*>(shape)->radius);
+      break;
     case CYLINDER:
-	result = new Cylinder(static_cast<const Cylinder*>(shape)->radius, static_cast<const Cylinder*>(shape)->length);
-	break;
+      result = new Cylinder(static_cast<const Cylinder*>(shape)->radius, static_cast<const Cylinder*>(shape)->length);
+      break;
     case BOX:
-	result = new Box(static_cast<const Box*>(shape)->size[0], static_cast<const Box*>(shape)->size[1], static_cast<const Box*>(shape)->size[2]);
-	break;
+      result = new Box(static_cast<const Box*>(shape)->size[0], static_cast<const Box*>(shape)->size[1],
+                       static_cast<const Box*>(shape)->size[2]);
+      break;
     case MESH:
-	{
-	    const Mesh *src = static_cast<const Mesh*>(shape);
-	    Mesh *dest = new Mesh(src->vertexCount, src->triangleCount);
-	    unsigned int n = 3 * src->vertexCount;
-	    for (unsigned int i = 0 ; i < n ; ++i)
-		dest->vertices[i] = src->vertices[i];
-	    n = 3 * src->triangleCount;
-	    for (unsigned int i = 0 ; i < n ; ++i)
-	    {
-		dest->triangles[i] = src->triangles[i];
-		dest->normals[i] = src->normals[i];
-	    }
-	    result = dest;
-	}
-	break;
-    default:
-	break;
-    }
-    return result;
-}
-
-shapes::StaticShape* shapes::cloneShape(const shapes::StaticShape *shape)
-{
-    shapes::StaticShape *result = NULL;
-    switch (shape->type)
     {
-    case PLANE:
-	result = new Plane(static_cast<const Plane*>(shape)->a, static_cast<const Plane*>(shape)->b, 
-			   static_cast<const Plane*>(shape)->c, static_cast<const Plane*>(shape)->d);
-	break;
-    default:
-	break;
+      const Mesh* src = static_cast<const Mesh*>(shape);
+      Mesh* dest = new Mesh(src->vertexCount, src->triangleCount);
+      unsigned int n = 3 * src->vertexCount;
+      for (unsigned int i = 0; i < n; ++i)
+        dest->vertices[i] = src->vertices[i];
+      n = 3 * src->triangleCount;
+      for (unsigned int i = 0; i < n; ++i)
+      {
+        dest->triangles[i] = src->triangles[i];
+        dest->normals[i] = src->normals[i];
+      }
+      result = dest;
     }
-    
-    return result;
+    break;
+    default:
+      break;
+  }
+  return result;
 }
 
+shapes::StaticShape* shapes::cloneShape(const shapes::StaticShape* shape)
+{
+  shapes::StaticShape* result = NULL;
+  switch (shape->type)
+  {
+    case PLANE:
+      result = new Plane(static_cast<const Plane*>(shape)->a, static_cast<const Plane*>(shape)->b,
+                         static_cast<const Plane*>(shape)->c, static_cast<const Plane*>(shape)->d);
+      break;
+    default:
+      break;
+  }
+
+  return result;
+}
 }
